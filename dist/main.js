d72eb8f5794419737fa745a22f8626e8ce899d55
Queenshiba <haruka0111.2a@gmail.com>
2021-06-20 22:54:23 +0900

    add filteringSentences()

--- dist/main.js
+++ dist/main.js
@@ -8,17 +8,43 @@ fetch("./data.json")
         const index = data.index;
 
         const result = document.getElementById('result')
-        
-index.forEach(element => {
 
-    let createPtag = document.createElement('p')
-    let samplesentence = element.sentence
-    let textnode = document.createTextNode(samplesentence)
-    console.log(samplesentence)
-    createPtag.appendChild(textnode)
-    result.appendChild(createPtag)
-});
-      
+        index.forEach(element => {
+            let createPtag = document.createElement('p')
+            let samplesentence = element.sentence
+            let textnode = document.createTextNode(samplesentence)
+            // console.log(samplesentence)
+            createPtag.appendChild(textnode)
+            result.appendChild(createPtag)
+        });
+
         console.log(index)
+
+        searchInputBtn.addEventListener('click', () => filteringSentences())
+        function filteringSentences() {
+            console.log(searchInput.value)
+            index.forEach(element => {
+                let samplesentence = element.sentence
+                if (samplesentence.includes(searchInput.value)) {
+                console.log(samplesentence)
+                } else {
+                    console.log("no")
+                }
+
+            });
+
+        }
     });
 
+
+
+let searchInput = document.getElementById("search-input")
+let searchInputBtn = document.getElementById("search-input-btn")
+
+
+
+
+
+// an affirmative sentence
+// a negative sentence
+// an interrogative sentence
