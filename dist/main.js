8b0127f50e5196ccd6fa93b966e8e65bb55b9276
Queenshiba <haruka0111.2a@gmail.com>
2021-06-28 23:50:06 +0900

    filteringCheckboxLabels

--- dist/main.js
+++ dist/main.js
@@ -53,9 +53,24 @@ fetch("./data.json")
     });
 
 
-
 let searchInput = document.getElementById("search-input")
 let searchInputBtn = document.getElementById("search-input-btn")
+
+//get lebels of checkbox
+searchInputBtn.addEventListener('click', () => filteringCheckboxLebels(e))
+function filteringCheckboxLabels(e) {
+    let labels = document.getElementsByTagName("label")
+    let text = []
+    console.log(labels.length)
+    for (let i = 0; i < labels.length; i++) {
+let label = labels[i];
+if(label.getAttribute("for") == e) {
+
+}
+
+    }
+}
+
 
 // createCheckboxLebel
 const typesofsentences = ["SV", "SVC", "SVO", "SVOO", "SVOC"]
@@ -63,6 +78,7 @@ let checkboxWrapType = document.getElementById('group-type')
 typesofsentences.forEach(element => {
     let createCheckbox = document.createElement('input')
     createCheckbox.setAttribute("type", "checkbox")
+    createCheckbox.setAttribute("for", element)
     let createCheckboxLebel = document.createElement('label')
     let typesofsentencesTextnode = document.createTextNode(element)
     createCheckboxLebel.appendChild(typesofsentencesTextnode)
@@ -70,13 +86,14 @@ typesofsentences.forEach(element => {
     checkboxWrapType.appendChild(createCheckboxLebel)
 })
 
-const typesoftenses = ["Present simple","Present tense", "Present continuous", "Present perfect continuous", "Past simple","Past tense", "Past continuous", "Present perfect", "Past perfect", "Past perfect continuous", "Future simple","Future simple", "Future continuous", "Future perfect", "Future perfect continuous"]
+const typesoftenses = ["Present simple", "Present tense", "Present continuous", "Present perfect continuous", "Past simple", "Past tense", "Past continuous", "Present perfect", "Past perfect", "Past perfect continuous", "Future simple", "Future simple", "Future continuous", "Future perfect", "Future perfect continuous"]
 const typesoftensesJp = { "現在時制": ["現在形", "現在進行形", "現在完了進行形"], "過去時制": ["過去形", "過去進行形", "現在完了形", "過去完了", "過去完了進行形"], "未来時制": ["未来形", "未来進行形", "未来完了形", "未来完了進行形"] }
 
 let checkboxWrapTense = document.getElementById('group-tense')
 typesoftenses.forEach(element => {
     let createCheckbox = document.createElement('input')
     createCheckbox.setAttribute("type", "checkbox")
+    createCheckbox.setAttribute("for", element)
     let createCheckboxLebel = document.createElement('label')
     let typesofsentencesTextnode = document.createTextNode(element)
     createCheckboxLebel.appendChild(typesofsentencesTextnode)
@@ -84,9 +101,9 @@ typesoftenses.forEach(element => {
     checkboxWrapTense.appendChild(createCheckboxLebel)
 })
 
-const infinitive = ["noun","adjective","adverb"] 
-const gerund = ["none"] 
-const comparativeJp = ["原級比較","比較級","最上級"]
+const infinitive = ["noun", "adjective", "adverb"]
+const gerund = ["none"]
+const comparativeJp = ["原級比較", "比較級", "最上級"]
 // an affirmative sentence
 // a negative sentence
 // an interrogative sentence
